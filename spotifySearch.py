4a6239867dfb590dacb679a09485b3e1eb6cbce6
alexgeary37 <alexgeary37@gmail.com>
2024-08-14 14:30:20 +1200

    Make it search for popularity instead of followers

--- spotifySearch.py
+++ spotifySearch.py
@@ -15,23 +15,40 @@ client_secret = os.getenv('CLIENT_SEC')
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# Convert command-line arguments to integers
-min_followers = int(sys.argv[1])  # sys.argv[0] is the script name, so start with sys.argv[1]
-max_followers = int(sys.argv[2])
+# Error handling for command-line arguments
+if len(sys.argv) < 4:
+    print("Usage: python3 myscript.py <keyword> <min_popularity> <max_popularity>")
+    sys.exit(1)
 
-def search_for_artists(keyword, min_followers, max_followers):
-    results = sp.search(q=keyword, type='artist', limit=50)
-    artists = results['artists']['items']
-    
+# Command-line arguments
+keyword = sys.argv[1]
+min_popularity = int(sys.argv[2])
+max_popularity = int(sys.argv[3])
+
+def search_for_artists_by_popularity(keyword, min_popularity, max_popularity):
+    offset = 0
+    limit = 50
     found_artist = False
-    for artist in artists:
-        followers = artist['followers']['total']
-        if min_followers < followers < max_followers:
-            print(f"Artist: {artist['name']}, Followers: {followers}, URL: {artist['external_urls']['spotify']}")
-            found_artist = True
-    
-    if not found_artist:
-        print("No artists found within the specified follower range.")
 
-# main program
-search_for_artists(keyword='indie', min_followers=min_followers, max_followers=max_followers)
+    while True:
+        # Search for artists with pagination
+        results = sp.search(q=keyword, type='artist', limit=limit, offset=offset)
+        artists = results['artists']['items']
+
+        for artist in artists:
+            popularity = artist['popularity']
+            if min_popularity <= popularity <= max_popularity:
+                print(f"Artist: {artist['name']}, Popularity: {popularity}, URL: {artist['external_urls']['spotify']}")
+                found_artist = True
+
+        # Break the loop if no more artists are found
+        if len(artists) < limit:
+            break
+        
+        offset += limit  # Move to the next page of results
+
+    if not found_artist:
+        print("No artists found within the specified popularity range.")
+
+# Main program
+search_for_artists_by_popularity(keyword=keyword, min_popularity=min_popularity, max_popularity=max_popularity)
